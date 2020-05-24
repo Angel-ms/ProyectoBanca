526328d494b2bdfabf4cd43f40c497a003a0ebdf
alumno <alumno@DESKTOP-4MLIONK.localdomain>
2020-05-24 18:51:49 +0200

    Created Tarjeta.java

---
+++ src/pqt/Tarjeta.java
@@ -0,0 +1,40 @@
+package pqt;
+
+public class Tarjeta {
+	float limiteRetiradaCajero;
+	float limitePagoInternet;
+	int pagosRealizados;
+	
+	Tarjeta(){
+		limiteRetiradaCajero = 0;
+		limitePagoInternet = 0;
+		pagosRealizados = 0;
+	}
+
+	public Tarjeta(float limiteRetiradaCajero, float limitePagoInternet) {
+		this.limiteRetiradaCajero = limiteRetiradaCajero;
+		this.limitePagoInternet = limitePagoInternet;
+		pagosRealizados = 0;
+	}
+
+	public float getLimiteRetiradaCajero() {
+		return limiteRetiradaCajero;
+	}
+	public void setLimiteRetiradaCajero(float limiteRetiradaCajero) {
+		this.limiteRetiradaCajero = limiteRetiradaCajero;
+	}
+
+	
+	public float getLimitePagoInternet() {
+		return limitePagoInternet;
+	}
+	public void setLimitePagoInternet(float limitePagoInternet) {
+		this.limitePagoInternet = limitePagoInternet;
+	}
+
+	
+	public int getPagosRealizados() {
+		return pagosRealizados;
+	}
+	
+}
